7e51e6b89d28f3281e4d483f48b9fb94fe453372
jhamPac <cloud@kevia.me>
2020-10-27 15:23:53 -0700

    Complete state.go methods and structs

--- database/state.go
+++ database/state.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -13,6 +16,7 @@ type State struct {
 	dbFile *os.File
 }
 
+// NewStateFromDisk starts the ledger from the genesis
 func NewStateFromDisk() (*State, error) {
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -28,4 +32,78 @@ func NewStateFromDisk() (*State, error) {
 	for account, balance := range gen.Balances {
 		balances[account] = balance
 	}
+
+	f, err := os.OpenFile(filepath.Join(cwd, "database", "tx.db"), os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	state := &State{balances, make([]Tx, 0), f}
+
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var tx Tx
+		json.Unmarshal(scanner.Bytes(), &tx)
+
+		if err := state.apply(tx); err != nil {
+			return nil, err
+		}
+	}
+
+	return state, nil
+}
+
+// Add a tx to the state mempool
+func (s *State) Add(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+	s.txMempool = append(s.txMempool, tx)
+	return nil
+}
+
+// Persist mempool to disk
+func (s *State) Persist() error {
+	mempool := make([]Tx, len(s.txMempool))
+	copy(mempool, s.txMempool)
+
+	for i := 0; i < len(mempool); i++ {
+		txJSON, err := json.Marshal(s.txMempool[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err = s.dbFile.Write(append(txJSON, '\n')); err != nil {
+			return err
+		}
+
+		s.txMempool = append(s.txMempool[:i], s.txMempool[i+1:]...)
+	}
+	return nil
+}
+
+// Close references to the file
+func (s *State) Close() {
+	s.dbFile.Close()
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if s.Balances[tx.From] < tx.Value {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+
+	return nil
 }
