1f754bccfcf7838b44e1df4dc24bde05dda8f707
jhamPac <cloud@kevia.me>
2020-10-27 15:32:09 -0700

    Create genesis.go

---
+++ database/genesis.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type genesis struct {
+	Balances map[Account]uint `json:"balances"`
+}
+
+func loadGenesis(path string) (genesis, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return genesis{}, err
+	}
+
+	var g genesis
+	err = json.Unmarshal(content, &g)
+	if err != nil {
+		return genesis{}, nil
+	}
+	return g, nil
+}
