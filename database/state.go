e3fd360fa0254aaa515863859fd0cb11ec02793c
jhamPac <cloud@kevia.me>
2020-10-29 20:34:22 -0700

    Add hashing to the state logic

--- database/state.go
+++ database/state.go
@@ -2,18 +2,24 @@ package database
 
 import (
 	"bufio"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
+
+// Snapshot represents a content hash
+type Snapshot [32]byte
 
 // State is the main business logic of the ledger
 type State struct {
 	Balances  map[Account]uint
 	txMempool []Tx
 
-	dbFile *os.File
+	dbFile   *os.File
+	snapshot Snapshot
 }
 
 // NewStateFromDisk starts the ledger from the genesis
@@ -58,6 +64,11 @@ func NewStateFromDisk() (*State, error) {
 	return state, nil
 }
 
+// LatestSnapshot returns the current hash of the tx.db file
+func (s *State) LatestSnapshot() Snapshot {
+	return s.snapshot
+}
+
 // Add a tx to the state mempool
 func (s *State) Add(tx Tx) error {
 	if err := s.apply(tx); err != nil {
@@ -68,23 +79,31 @@ func (s *State) Add(tx Tx) error {
 }
 
 // Persist mempool to disk
-func (s *State) Persist() error {
+func (s *State) Persist() (Snapshot, error) {
 	mempool := make([]Tx, len(s.txMempool))
 	copy(mempool, s.txMempool)
 
 	for i := 0; i < len(mempool); i++ {
 		txJSON, err := json.Marshal(s.txMempool[i])
 		if err != nil {
-			return err
+			return Snapshot{}, err
 		}
 
+		fmt.Printf("Persisting new TX to disk:\n")
+		fmt.Printf("\t%s\n", txJSON)
 		if _, err = s.dbFile.Write(append(txJSON, '\n')); err != nil {
-			return err
+			return Snapshot{}, err
 		}
+
+		err = s.doSnapshot()
+		if err != nil {
+			return Snapshot{}, err
+		}
+		fmt.Printf("New DB Snapshot: %x\n", s.snapshot)
 
 		s.txMempool = append(s.txMempool[:i], s.txMempool[i+1:]...)
 	}
-	return nil
+	return s.snapshot, nil
 }
 
 // Close references to the file
@@ -104,6 +123,21 @@ func (s *State) apply(tx Tx) error {
 
 	s.Balances[tx.From] -= tx.Value
 	s.Balances[tx.To] += tx.Value
+
+	return nil
+}
+
+func (s *State) doSnapshot() error {
+	_, err := s.dbFile.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	txsData, err := ioutil.ReadAll(s.dbFile)
+	if err != nil {
+		return err
+	}
+	s.snapshot = sha256.Sum256(txsData)
 
 	return nil
 }
