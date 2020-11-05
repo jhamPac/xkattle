7c95f9ceb592a387cf5b2b5c8cea1c9437272c63
jhamPac <cloud@kevia.me>
2020-11-05 11:52:53 -0700

    Create cmd to migrate db to block.db

---
+++ cmd/xmigrate/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jhampac/xkattle/database"
+)
+
+func main() {
+	state, err := database.NewStateFromDisk()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer state.Close()
+
+	block0 := database.NewBlock(
+		database.Hash{},
+		uint64(time.Now().Unix()),
+		[]database.Tx{
+			database.NewTx("alice", "alice", 3, ""),
+			database.NewTx("alice", "alice", 700, "reward"),
+		},
+	)
+
+	state.AddBlock(block0)
+	block0hash, _ := state.Persist()
+
+	block1 := database.NewBlock(
+		block0hash,
+		uint64(time.Now().Unix()),
+		[]database.Tx{
+			database.NewTx("alice", "bob", 2000, ""),
+			database.NewTx("alice", "alice", 100, "reward"),
+			database.NewTx("bob", "alice", 1, ""),
+			database.NewTx("alice", "eve", 10000, ""),
+			database.NewTx("alice", "bob", 10000, ""),
+			database.NewTx("alice", "charlie", 1000, ""),
+			database.NewTx("alice", "charlie", 1000, ""),
+			database.NewTx("alice", "alice", 1000, "reward"),
+			database.NewTx("charlie", "charlie", 10000, "reward"),
+			database.NewTx("charlie", "charlie", 10000, "reward"),
+		},
+	)
+
+	state.AddBlock(block1)
+	state.Persist()
+}
