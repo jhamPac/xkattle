8585bb89cd52d19ab5d5b757cfecfdeed5c43097
jhamPac <cloud@kevia.me>
2020-11-10 09:21:36 -0700

    Save point before work

--- node/node.go
+++ node/node.go
@@ -1,6 +1,11 @@
 package node
 
-import "github.com/jhampac/xkattle/database"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/jhampac/xkattle/database"
+)
 
 const httpPort = 9000
 
@@ -22,4 +27,52 @@ type TxAddReq struct {
 
 type TxAddRes struct {
 	Hash database.Hash `json:"block_hash"`
+}
+
+func Run(dataDir string) error {
+	fmt.Println(fmt.Sprintf("Listening on HTTP port: %d", httpPort))
+
+	state, err := database.NewStateFromDisk(dataDir)
+	if err != nil {
+		return err
+	}
+	defer state.Close()
+
+	http.HandleFunc("/balances/list", func(w http.ResponseWriter, r *http.Request) {
+		listBalancesHandler(w, r, state)
+	})
+
+	http.HandleFunc("/tx/add", func(w http.ResponseWriter, r *http.Request) {
+		txAddHandler(w, r, state)
+	})
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+}
+
+func listBalacesHandler(w http.ResponseWriter, r *http.Request, state *database.State) {
+	writeRes(w, BalancesRes{state.LatestBlockHash(), state.Balances})
+}
+
+func txAddHandler(w http.ResponseWriter, r *http.Request, state *database.State) {
+	req := TxAddReq{}
+	err := readReq(r, &req)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	tx := database.NewTx(database.NewAccount(req.From), database.NewAccount(req.To), req.Value, req.Data)
+
+	err = state.AddTx(tx)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	hash, err := state.Persist()
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+	writeRes(w, TxAddRes{hash})
 }
