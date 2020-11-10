ce8e9e1b2d3c18f276af443816dd1c7ff310822c
jhamPac <cloud@kevia.me>
2020-11-09 20:41:36 -0700

    Save point before finishing implementing a node

---
+++ node/node.go
@@ -0,0 +1,25 @@
+package node
+
+import "github.com/jhampac/xkattle/database"
+
+const httpPort = 9000
+
+type ErrRes struct {
+	Error string `json:"error"`
+}
+
+type BalancesRes struct {
+	Hash     database.Hash             `json:"block_hash"`
+	Balances map[database.Account]uint `json:"balances"`
+}
+
+type TxAddReq struct {
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value uint   `json:"value"`
+	Data  string `json:"data"`
+}
+
+type TxAddRes struct {
+	Hash database.Hash `json:"block_hash"`
+}
